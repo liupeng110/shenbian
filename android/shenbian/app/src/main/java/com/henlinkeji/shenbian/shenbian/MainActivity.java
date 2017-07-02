92d5d701b1ca85d777ba9be07a529866f82704cf
chen_jian <417497299@qq.com>
2017-07-03 00:54:35 +0800

    add color inkiness.

--- android/shenbian/app/src/main/java/com/henlinkeji/shenbian/shenbian/MainActivity.java
+++ android/shenbian/app/src/main/java/com/henlinkeji/shenbian/shenbian/MainActivity.java
@@ -1,11 +1,11 @@
 package com.henlinkeji.shenbian.shenbian;
 
-import android.app.Activity;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,40 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(Build.VERSION.SDK_INT > 16) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    |View.SYSTEM_UI_LAYOUT_FLAGS|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-            getWindow().getDecorView().setFitsSystemWindows(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
         }
+
     }
-
-    public static void setStatusBarTranslucent(ViewGroup vg, Activity activity) {
-        // 如果版本在4.4以上
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            // 状态栏高度
-            int height = getStatusBarHeight(activity);
-            if (height <= 0) {
-                return;
-            }
-            // 设置距离顶部状态栏垂直距离
-            vg.setPadding(0, height, 0, 0);
-            // 状态栏透明
-            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            // 导航栏透明
-//            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        }
-    }
-
-    public static int getStatusBarHeight(Activity activity) {
-        int height = 0;
-        int resourceId = activity.getResources().getIdentifier("status_bar_height", "dimen",
-                "android");
-        if (resourceId > 0) {
-            height = activity.getResources().getDimensionPixelSize(resourceId);
-        }
-        return height;
-    }
-
-
 }
